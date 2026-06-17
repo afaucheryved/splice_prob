c900855950a6795dc4c38e52666dcd565132eb2a
afaucheryved <florian.malard@gmail.com>
2026-06-17 11:40:32 +0200

    new docstring alteration_functions.py

--- app/domain/alteration_functions.py
+++ app/domain/alteration_functions.py
@@ -3,78 +3,141 @@
 #local import
 from app.schemas.typing import *
 
-class GeneralterartionFonctions:
+class AlterartionFunctionsByIndex:
 
     """
-    this class of functions is about to work on patterns' atcg sequence.
-    they return the the sequence
+    This class of functions is about to work on patterns' atcg sequence.
+    They return the new sequence.
     """
 
-    def remplace_pattern(sequence: genome, 
-                            olde_pattern: str | None=None, 
-                            new_pattern :str | None=None, 
-                            index_start : str | None=None, 
-                            index_end : str | None=None)-> genome:
+    def place_pattern(sequence: genome, 
+                            pattern :str, 
+                            index :int,
+                            lenght :int=0 | str)-> genome:
         """
-        remplace a pattern of bases by another one
-        Indices can be used if one works with indices rather than replacing each recognized pattern.
+        Place an actg pattern at an index a over a specified length of the sequence (instead of another one if lenght != 0).
+        If 'length' = ":", then the length is the distance from the index to the end of the sequence. 
+        Example:
+                pattern = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", lenght = 16
+
+                                    PLACE (instead of)
+            -> ...atcgatcgatcgatcgatcgatcgatcgatcgatcgatcgatcgatcgatcgatcgatcg...
+                                    |----------------|
+                                   index    (lenght) |_____
+                                    |                      |_____
+                                    |                            |____
+                                    |aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa |
         """
         return 
     
-    def delete_pattern(sequence: genome, pattern: str, 
-                       index_start : str | None=None, 
-                       index_end : str | None=None)-> genome:
+    def delete_pattern(sequence: genome, 
+                       start : int, 
+                       end : int | None=None,
+                       lenght: int | str | None=None)-> genome:
         """
-        delet all patters from a sequence
-        Indices can be used if one works with indices rather than replacing each recognized pattern.
+        Delet the bases beteen the position 'start' and 'end'.
+        You can use 'lenght' parametter instead of 'end'.
+        If 'length' = ":", then the length is the distance from the index to the end of the sequence.
+        Example:
+                        
+                                        DELETED
+            -> ...atcgatcgatcgatcgatcgatcgatcgatcgatcgatcgatcgatcgatcgatcgatcg...
+                                    |----------------|
+                                   start  (lenght)  end
         """
         return
 
-    def add_pattern(sequence: genome, pattern: str,
-                    index: int | None=None, 
-                    left_neighbor_pattern :str | None=None,
-                    right_neighbor_pattern :str | None=None)-> genome:
-        """
-        add a pattern at a specific place in the genome
-        Here, you either place the pattern at the beginning or end of a neighbor pattern, 
-        or you specify the index where you want to place the desired pattern.
-
-        exemple :
-            if :
-                sequence = "...atatatatatcggcatatatatatat..."
-                pattern = "_NEW_PATTERN_"
-                left_n_p = "cg"
-                right_n_p = "gc"
-            then :
-                --> "...atatatatatcg_NEW_PATTERN_gcatatatatatat..."
-        """
-        return
     def cc_past(sequence: genome,
-                     start_cc_index: int,
-                     end_cc_index: int,
-                     start_past_index: int,
-                     end_past_index: int,
-                     mode: str="copy")-> genome:
+                     start_cc: int,
+                     end_cc: int,
+                     index_past: int,
+                     lenght_past: int=0,
+                     mode: str="cut")-> genome:
         """
-        cut (or copy) and past a sequence
+        Cut (or copy) and past a sequence.
+        Example:
+                        
+                      COPY / CUT                                PAST
+            -> ...atcgatcgatcgatcgatcgatcgatcgatcgatcgatcgatcgatcgatcgatcgatcg...
+                    |-----------|                      |---------------------|
+                 start_cc     end_cc               index_past   (lenght_past)                                                  
+                        
+
         """
 
-class NewSequenceFonctions:
+class AlterartionFunctionsByPattern:
 
     """
-    creating a sequence from scratch
+    This class of functions is about to work on patterns' atcg sequence.
+    The entire input sequence is processed by the functions.
+    They return the new sequence.
+    """
+
+    def replace_pattern(sequence: genome, 
+                            olde: str, 
+                            new:str, )-> genome:
+        """
+        Replace a pattern of bases by another one
+        ! -> "_" is a wildcard character (replaces 1 atcg base).
+        ! -> "%(n)" is a wildcard character (replaces any long sequence of up to 'n' ATCG bases).
+        Example:
+
+            old = "cc_c", new = "aaaa"
+                               REPLACE                  REPLACE
+            -> ...atcgatcgatcgatccccgatcgatcgatcgatcgatcgatcctcgatcgatcgatcgatcgatcg...
+                                |--|                       |--|
+                                aaaa                       aaaa
+                                
+
+        """
+        return 
+    
+    def delete_pattern(sequence: genome, 
+                       pattern: str, 
+                       )-> genome:
+        """
+        ! -> "_" is a wildcard character (replaces 1 atcg base).
+        ! -> "%(n)" is a wildcard character (replaces any long sequence of up to 'n' ATCG bases).
+        Example:
+
+            pattern = "cc_c"
+                               DELETE                      DELET
+            -> ...atcgatcgatcgatccccgatcgatcgatcgatcgatcgatcctcgatcgatcgatcgatcgatcg...
+                                |--|                       |--|
+                                aaaa                       aaaa
+                                
+
+        """
+        return
+
+class NewSequenceFunctions:
+
+    """
+    Generating a sequence.
     """
     def repeat(pattern: str, nbr: int, 
                 start_pattern: str="", 
                 end_pattern: str="")-> genome:
         """
-        repeats a pattern a number of times, with a non-recursive pattern at the beginning and end
+        Repeats a pattern a number of times, with a non-recursive pattern at the beginning and end.
+        Example:
+
+            pattern = "atcg", nbr = 10, start_pattern = "aaaa", end_attern = "cccc"
+            
+            --> aaaaatcgatcgatcgatcgatcgatcgatcgatcgatcgatcgcccc
+            <start> |              <lopp>                   | <end>
         """
         return
     
-    def merge(sequensess :list[genome])-> genome:
+    def merge(sequenses :list[genome])-> genome:
         """
-        return merged sequences
+        Return merged sequences.
+
+        Example:
+
+            sequences = ["aaaa", "tttt", "cccc", "gggg"]
+            
+            --> aaaattttccccgggg
         """
         return
 
