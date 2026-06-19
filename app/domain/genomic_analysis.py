b2b9b134bdff060d4c39424b94b760ac5638f453
afaucheryved <afaucheryved@ensc.fr>
2026-06-19 18:40:13 +0200

    check functions genom

--- app/domain/genomic_analysis.py
+++ app/domain/genomic_analysis.py
@@ -1,27 +1,31 @@
 #general import
 
 #local import
+from app.schemas.typing import mut
 from app.schemas.typing import *
 
 class ImportanceSplicingSearch:
     """
-    Functions that identify the patterns and locations having the greatest impact on splicing probabilities changes.
+    Have a function that identify the patterns and locations having the greatest impact on splicing probabilities changes.
     Returns a dictionary mapping each position to an importance score regarding the splicing process.
     """
 
-    def zona(sequence: genome,
-                threshold: float) -> list[list[int]]:
+    @staticmethod
+    def _zona(sequence: genome,
+                threshold: float, 
+                mutation_intensity: int = 10) -> list[list[int]]:
         """
         It returns, as a list of pairs, the boundaries of the intervals corresponding to regions that significantly impact splicing probabilities (segment/change-point detection), using "ruptures" library.
-        To achieve this, the function introduces random mutations every X bases, calculates the delta score, and repeats the process multiple times.
+        To achieve this, the function introduces random mutations every N='mutation_intensity' bases, calculates the delta score, and repeats the process multiple times.
         The results are then combined on a per-base basis and randomized, allowing for the identification of regions where values exceed the threshold.
         """
         return
 
+    @staticmethod
     def pattern_in_zona(sequence: genome, 
-                            zona_intervales: list[list[int]]):
+                            zona_intervales: list[list[int]]) -> dict[set[mut], float] :
         """
-        Uses 'enumerate_window_mutants' followed by 'calcul_y' and 'Scorng.mut', 
+        Uses 'enumerate_window_mutants' followed by 'calcul_y' and 'Scoring.mut', 
         to identify the patterns most significant for altering the splicing score within the previously identified regions of importance.
         """
         return
--- app/test/test_functions.py
+++ app/test/test_functions.py
@@ -2,7 +2,7 @@
 from functools import wraps
 
 #local import
-from app.domain.sequence_functions import WindowMutationFunctions
+from app.domain.sequence_functions import *
 
 # tool test
 def print_dic_lisible(func):
@@ -18,15 +18,36 @@ def print_dic_lisible(func):
     return wrapper
 
 # var test
-seq = "atcgatcg"
+seq = "atcgatcgatcgatcgatcg" # 6
 
 window = 4
 
 start, end, step = 0, len(seq), 4
 
-# function test
-@print_dic_lisible
-def f():
-    return WindowMutationFunctions.enumerate_window_mutants(seq, start, end, step, window)
+# functions to test
 
-f()
+# insert_patter
+#delet_pattern
+#move_pattern
+#copy_past_pattern
+#replace pattern
+#delete-pattern
+# repeat
+#merge
+# mutate_independently
+# enumerate_window_mutants
+"""
+change : 
+index must start at 1 for all functions
+
+"""
+
+#print(AlterationByIndexFunctions.insert_pattern(seq, "NNNN", 1)) # NNNNatcg... # index start 1
+#print(AlterationByIndexFunctions.delete_pattern("atcgatcg", 1, 4)) # index start 1
+#print(AlterationByIndexFunctions.move_pattern("NNNNatcgatcg", 1, 4, 4)) #index start 1
+#print(AlterationByIndexFunctions.copy_past_pattern("NEWPatcgatcg", 1, 4, 4)) # index start 1
+#print(AlterationByPatternFunctions.replace_pattern(seq, "atcg", "gcta")) #ok
+#print(AlterationByPatternFunctions.delete_pattern(seq, "a")) #ok
+#print(SequenceFactory.repeat("atcg", 2)) #ok
+#print(SequenceFactory.merge(["atcg", "atcg"])) #ok
+
